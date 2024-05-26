cbafb35a2a6f8b274fd7d9c33a7c5399556b53bf
NyanDesigns <nmyat@cpp.edu>
2024-05-26 16:56:56 -0700

    vid-gif

--- app/Home.tsx
+++ app/Home.tsx
@@ -2,21 +2,22 @@
 
 import { FFmpeg } from '@ffmpeg/ffmpeg'
 import { fetchFile, toBlobURL } from '@ffmpeg/util'
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 
 export default function Home() {
   const [loaded, setLoaded] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  const [video, setVideo] = useState<File | null>()
+  const [processing, setProcessing] = useState(false)
   const ffmpegRef = useRef(new FFmpeg())
-  const videoRef = useRef<HTMLVideoElement | null>(null)
+  const gifRef = useRef<HTMLImageElement | null>(null)
   const messageRef = useRef<HTMLParagraphElement | null>(null)
 
   const load = async () => {
-    setIsLoading(true)
     const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd'
     const ffmpeg = ffmpegRef.current
     ffmpeg.on('log', ({ message }) => {
       if (messageRef.current) messageRef.current.innerHTML = message
+      console.log(message);
     })
     // toBlobURL is used to bypass CORS issue, urls with the same
     // domain can be used directly.
@@ -25,52 +26,79 @@ export default function Home() {
       wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm')
     })
     setLoaded(true)
-    setIsLoading(false)
   }
 
   const transcode = async () => {
     const ffmpeg = ffmpegRef.current
-    // u can use 'https://ffmpegwasm.netlify.app/video/video-15s.avi' to download the video to public folder for testing
-    await ffmpeg.writeFile('input.avi', await fetchFile('https://raw.githubusercontent.com/ffmpegwasm/testdata/master/video-15s.avi'))
-    await ffmpeg.exec(['-i', 'input.avi', 'output.mp4'])
-    const data = (await ffmpeg.readFile('output.mp4')) as any
-    if (videoRef.current)
-      videoRef.current.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }))
+    setProcessing(true);
+    if (video){
+        try {
+            await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+            await ffmpeg.exec(['-i', 'input.mp4', 'output.gif'])
+            const data = (await ffmpeg.readFile('output.gif')) as any
+            if (gifRef.current)
+              gifRef.current.src = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }))
+        } catch {
+            console.error("The following error occured while transcoding: ", Error)
+        } finally {
+            setProcessing(false)
+        }
+    }
   }
 
+  useEffect(() => {
+        load();
+  }, []);
+
   return loaded ? (
-    <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-      <video ref={videoRef} controls></video>
-      <br />
-      <button
-        onClick={transcode}
-        className="bg-green-500 hover:bg-green-700 text-white py-3 px-6 rounded"
-      >
-        Transcode avi to mp4
-      </button>
-      <p ref={messageRef}></p>
+    //heroSection
+    <div className="w-screen h-screen flex justify-center items-center">
+        <div className='flex-col w-fit h-fit'>
+            <h1 className='text-center text-3xl text-gray-300 font-semibold'>Video to GiF</h1>
+            <br/>
+            
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">
+                Upload file
+            </label>
+            <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
+                aria-describedby="file_input_help" 
+                id="file_input" 
+                type="file"
+                accept='video/*'
+                onChange={(e) => setVideo(e.target.files?.[0] || null)}
+            />
+            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">
+                SVG, PNG, JPG or GIF (MAX. 800x400px).
+            </p>
+            <video
+                controls
+                width={"250"}
+                src={video ? URL.createObjectURL(video) : URL.createObjectURL(new Blob())}
+            ></video>
+            <br />
+            
+            <button
+                onClick={transcode}
+                disabled={processing}
+                type="button"
+                className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+                {processing ? ('Processing...') : ('Process Video')}
+            </button>
+            <p className="text-center" ref={messageRef}></p>
+            <br />
+
+            <img src="" ref={gifRef} alt="" width="250"/>
+        </div>
     </div>
   ) : (
-    <button
-      className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex items-center bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
-      onClick={load}
-    >
-      Load ffmpeg-core
-      {isLoading && (
-        <span className="animate-spin ml-3">
-          <svg
-            viewBox="0 0 1024 1024"
-            focusable="false"
-            data-icon="loading"
-            width="1em"
-            height="1em"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path d="M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z"></path>
-          </svg>
-        </span>
-      )}
-    </button>
+    //loadingPage
+    <div>
+    <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+    </svg>
+    Loading...
+    </div>
   )
 }
